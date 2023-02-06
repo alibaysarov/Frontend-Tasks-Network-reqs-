7fbee4520505ed32c78937930db05cae994299d6
alibaysarov <alibajsarov353@gmail.com>
2023-02-07 00:22:52 +0300

    Отправка запроса ч/з fetch (async/await)

--- index.js
+++ index.js
@@ -8,25 +8,33 @@
 // }
 
 // Добавление при помощи цепочки промисов
-function fakeFetch(url, method = 'GET') {
-  function success() {
-    let xhr = new XMLHttpRequest();
-    xhr.open(method.toUpperCase(), url);
-    xhr.responseType = 'json';
-    xhr.send();
-    return xhr;
-  }
-  const errHandler = () => console.log('Ошибка');
-  return new Promise(function (resolve, reject) {
-    resolve(success);
-    reject(errHandler);
-  });
+// function fakeFetch(url, method = 'GET') {
+//   function success() {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open(method.toUpperCase(), url);
+//     xhr.responseType = 'json';
+//     xhr.send();
+//     return xhr;
+//   }
+//   const errHandler = () => console.log('Ошибка');
+//   return new Promise(function (resolve, reject) {
+//     resolve(success);
+//     reject(errHandler);
+//   });
+// }
+// fakeFetch('https://jsonplaceholder.typicode.com/todos/10')
+//   .then(res=>res())
+//   .then(res=>{
+//     res.onload=function () { 
+//       console.log(res.response);
+//     }
+//   })
+
+async function fetchWithAwait (url) { 
+let res= await fetch(url);
+ if(res.ok){
+  let json= await res.json()
+  console.log(json);
+ }
 }
-fakeFetch('https://jsonplaceholder.typicode.com/todos/10')
-  .then(res=>res())
-  .then(res=>{
-    res.onload=function () { 
-      console.log(res.response);
-    }
-  })
-    
+fetchWithAwait('https://jsonplaceholder.typicode.com/todos/10')
